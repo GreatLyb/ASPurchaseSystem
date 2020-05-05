b9eefd7dcc2dcc5d14d00834cf57eae481df52d7
Lyb <1778934152@qq.com>
2020-05-06 07:20:03 +0800

    --

---
+++ app/src/main/java/com/lyb/purchasesystem/bean/PurchaseBean.kt
@@ -0,0 +1,23 @@
+package com.lyb.purchasesystem.bean
+
+/**
+ * ASPurchaseSystem
+ * 类描述：
+ * 类传参：
+ * @Author： create by Lyb on 2020/5/3 15:31
+ */
+class PurchaseBean {
+
+    lateinit var purchaseName:String//采购名称
+    lateinit var purchasespecifications:String//采购类型
+    lateinit var purchasePrice:String//采购价格
+    lateinit var purchaseNum:String//采购数量
+    lateinit var purchaseUseTime:String//采购使用时间
+    lateinit var purchasePublishTime:String//采购发布时间
+    lateinit var purchasedealTime:String//采购处理时间
+
+
+
+
+
+}
